278f2a9d4c40914f922aaa6ad0c8e8a98cce35d0
Truepolar <ryansoh.mail@gmail.com>
2025-09-02 12:51:59 +0800

    FIXED : placement still need to add conflict detection

--- placement.py
+++ placement.py
@@ -12,14 +12,16 @@ class Placement:
     @staticmethod
     def place_ship(name):
         
-        
+##HOW MANY SHIPS
 
         print("How many ships would you like to play with? (3-5)")
         number_of_ships = int(input())
         
-        
-            
-        for i in range(number_of_ships):
+        ship_counter = 1
+
+##START LOOP AND ASKING VERT OR HORI
+
+        while ship_counter <= number_of_ships:
             ship_length = 0
             row_coordinate = []
             column_coordinate = []
@@ -29,6 +31,10 @@ class Placement:
                 orientation = int(input())
                 if (orientation == 0 or orientation == 1):
                     check = False
+                else:
+                    print("------------ invalid input, try again ------------")
+
+##ASKING FOR ROW AND COLUMN
 
             check2 = True
             while(check2 == True): 
@@ -36,44 +42,48 @@ class Placement:
                 ship_placement_column = int(input())
                 print("Enter the row number of the desired ship placement (1-10)\n")
                 ship_placement_row = int(input())
-                if 1<ship_placement_column<11 and 1<ship_placement_row<11:
+                if 0<ship_placement_column<11 and 0<ship_placement_row<11:
                     check2 = False
+
+                else:
+                    print("------------ invalid input, try again ------------")
+
+##ASKING FOR LENGTH
 
             while(ship_length <= 2 or ship_length >= 6):
                 print("Enter the desired ship length (3-5)\n")
                 ship_length = input()
                 ship_length = int(ship_length)
-                print(ship_length)
 
                 i = 1
 
-            if (orientation == "0"):
+ ## THE 2 CHUNKS UNDER IS PROCESSING FOR PLACEMENT
+
+            if (orientation == 0):
                 while i <= ship_length:
                     temp_row = i + ship_placement_row -1
                     row_coordinate.append(temp_row)
                     column_coordinate.append(ship_placement_column)
-                    print(row_coordinate)
-                    print(column_coordinate)
                     check2 = False
                     i += 1
                     
-            elif (orientation == "1"):
+            elif (orientation == 1):
                 while i <= ship_length:
                     temp_column = i + ship_placement_column -1
                     row_coordinate.append(temp_column)
                     column_coordinate.append(ship_placement_column)
-                    print(row_coordinate)
-                    print(column_coordinate)
                     check2 = False
                     i += 1
-                
+
+##APPEND TO OBJECT LIST
+
             else:
                 check2 = True
             print(f"Ship {i}, \n row :{row_coordinate} \n column{column_coordinate}")
             ship = Ship(True , row_coordinate , column_coordinate)
             player = Player(name)
             player.add_ship(ship)
-                             
+            ship_counter += 1
         
 
 
